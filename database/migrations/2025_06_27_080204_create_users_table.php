9265df602390f28f1544bb711a48d79942aabe6d
avidvesha <apit.scorpio10@gmail.com>
2025-06-27 15:57:43 +0700

    database migration, undone fe: @damar, @aya

---
+++ database/migrations/2025_06_27_080204_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('username');
+            $table->string('email');
+            $table->string('password');
+            $table->date('date_of_birth');
+            $table->string('gender');
+            $table->text('address');
+
+            $table->unsignedBigInteger('resume_id');
+            $table->unsignedBigInteger('role_id');
+
+            $table->foreign('resume_id')->references('id')->on('resumes');
+            $table->foreign('role_id')->references('id')->on('roles');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/migrations/2025_06_27_081332_create_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('location');
+            $table->integer('salary');
+            $table->date('date_uploaded');
+            $table->boolean('status');
+
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
