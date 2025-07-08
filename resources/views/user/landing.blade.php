7c7e6569ae84c9c17445b92735c935e4a9a7bc65
admin <irfanprayoga29@gmail.com>
2025-07-08 20:56:58 +0700

    fix route

--- resources/views/user/landing.blade.php
+++ resources/views/user/landing.blade.php
@@ -171,7 +171,7 @@
                         <h5>Complete Profile</h5>
                         <p class="text-muted mb-3">Make your profile stand out</p>
                         <!-- <a href="{{ route('user.profile.edit') }} " class="btn btn-outline-primary">Edit Profile</a> -->
-                        <a href="{{ route('user.profile.edit') }}" class="btn btn-outline-primary">Edit Profile</a>                          
+                        <a href="{{ route('user.profile') }}" class="btn btn-outline-primary">Edit Profile</a>                          
                     </div>
                 </div>
                 
